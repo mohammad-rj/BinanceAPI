2af547eba625afea581ebed879cab60996477e63
chico3001 <javier.ortegal@gmail.com>
2021-04-28 10:45:23 -0500

    first commit

---
+++ database/seeders/SecuencerSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SecuencerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
